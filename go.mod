967bd3c183162e44bfa0b54fe067359041621a62
UndeadTokenArt <undeadtokenart@gmail.com>
2023-10-23 15:30:39 -0700

    boilerplate for starting a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/undeadtokenart/chirpy
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
+
+	fmt.Println("Server is running on http://localhost:8080")
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
